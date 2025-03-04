659ca4485952852d6bff0003c54c26129fc1eadb
SohailaN12 <sohaila.noori2027@gbwl.org>
2025-03-04 17:57:04 +0000

    added-JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+/* This is a MULTI-LINE COMENT
+the code below is an example STATEMENT (instruction/command) Every STATEMENT ends in a SEMICOLON(;) */
+
+console.log("welcome to JavaScript");
+
+
+// This is a SINGLE-LINE COMMENT
+
+// console.log() is a FUNCTION that displays info
+
+console.log("My name is Sohaila");
