14def8e440f7a3741c69d90e7f18b54059441155
SohailaN12 <sohaila.noori2027@gbwl.org>
2025-03-06 18:37:45 +0000

    added-JavaScript

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ console.log("welcome to JavaScript");
 // console.log() is a FUNCTION that displays info
 
 console.log("My name is Sohaila");
+
+// DECLARE a new VARIABLE to hold some date 
+let pokemon;
+
+// ASSING a VALUE to that variable 
+// like putting an item in a box 
+pokemon = "pikachu";
+
+// DECLARE + ASSING in one statement 
+let luckyNumber = 6;
+
+//"PASS IN" a variable to FUNCTION 
+console.log(luckyNumber);
+
+// CHANGE the value of an existing variable 
+let raining = false;
+console.log(raining);
+raining = true;
+console.log(raining);
+
+// COPY data from one variable into another 
+let breakfast = "cerelac"
+let lunch;
+lunch = breakfast
+// Both variables hold the same value
+console.log(breakfast)
+console.timeLog(lunch)
+
